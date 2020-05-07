1062790964a90f8d1aee15d70f538ad40afa550e
jamie.sgro <iamjamies@gmail.com>
2020-05-07 14:58:09 -0400

    added basic UI

--- src/WPFDesktopUI/ViewModels/ShellViewModel.cs
+++ src/WPFDesktopUI/ViewModels/ShellViewModel.cs
@@ -1,10 +1,74 @@
-﻿using System;
+﻿using Caliburn.Micro;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WPFDesktopUI.Models;
 
 namespace WPFDesktopUI.ViewModels {
-  public class ShellViewModel {
-  }
+  public class ShellViewModel : Screen {
+		private string _firstName = "Jamie";
+		private string _lastName;
+		private BindableCollection<PersonModel> _people = new BindableCollection<PersonModel>();
+		private PersonModel _seletedPerson;
+
+		public ShellViewModel() {
+			People.Add(new PersonModel { FirstName = "Amy", LastName = "Adams" });
+			People.Add(new PersonModel { FirstName = "Bill", LastName = "Bobert" });
+			People.Add(new PersonModel { FirstName = "Cory", LastName = "Chase" });
+		}
+
+		public string FirstName {
+			get { 
+				return _firstName;
+			}
+			set {
+				_firstName = value;
+				NotifyOfPropertyChange(() => FirstName);
+				NotifyOfPropertyChange(() => FullName);
+			}
+		}
+
+
+		public string LastName {
+			get { return _lastName; }
+			set { 
+				_lastName = value;
+				NotifyOfPropertyChange(() => LastName);
+				NotifyOfPropertyChange(() => FullName);
+			}
+		}
+
+		public string FullName{
+			get { return $"{FirstName} {LastName}"; }
+		}
+
+
+		public BindableCollection<PersonModel> People {
+			get { return _people; }
+			set { _people = value; }
+		}
+
+
+		public PersonModel SelectedPerson {
+			get { return _seletedPerson; }
+			set {
+				_seletedPerson = value;
+				NotifyOfPropertyChange(() => SelectedPerson);
+			}
+		}
+
+		public bool CanClearText(string firstName, string lastName) {
+			if (string.IsNullOrWhiteSpace(firstName) && string.IsNullOrWhiteSpace(lastName)) {
+				return false;
+			}
+			return true;
+		}
+
+		public void ClearText(string firstName, string lastName) {
+			FirstName = "";
+			LastName = "";
+		}
+	}
 }
