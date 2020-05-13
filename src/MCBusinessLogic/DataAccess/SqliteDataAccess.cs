408b48a8a7422d5743e90f61b583e6c0584c3e2f
jamie.sgro <iamjamies@gmail.com>
2020-05-13 12:12:19 -0400

    added sqlite and data access files

---
+++ src/MCBusinessLogic/DataAccess/SqliteDataAccess.cs
@@ -0,0 +1,31 @@
+﻿using Dapper;
+using MCBusinessLogic.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCBusinessLogic.DataAccess {
+  public class SqliteDataAccess {
+    private static string LoadConnectionString(string id = "Default") {
+      return ConfigurationManager.ConnectionStrings[id].ConnectionString;
+    }
+
+    public static List<CustomerModel> LoadData() {
+      using (IDbConnection cnn = new SQLiteConnection(LoadConnectionString())) {
+        var output = cnn.Query<CustomerModel>("SELECT * FROM customer", new DynamicParameters());
+        return output.ToList();
+      }
+    }
+
+    public static void SaveData(CustomerModel customer) {
+      using (IDbConnection cnn = new SQLiteConnection(LoadConnectionString())) {
+        cnn.Execute("INSERT INTO customer (StaffName, Quantity) VALUES (@StaffName, @Quantity)", customer);
+      }
+    }
+  }
+}
