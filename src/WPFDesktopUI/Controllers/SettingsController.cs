97a50d9be55b55be6aab9e112e940be28916dae0
jamie.sgro <iamjamies@gmail.com>
2020-06-01 12:36:23 -0400

    added setting controller to deal with accessing settings

---
+++ src/WPFDesktopUI/Controllers/SettingsController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFDesktopUI.Controllers {
+  internal static class SettingsController {
+    public static string GetQbFilePath() {
+      return Properties.Settings.Default["StnQbFilePath"].ToString();
+    }
+  }
+}
