296bdc667256f2ff0e3d3077c9fa28655647b5c1
jamie.sgro <iamjamies@gmail.com>
2020-06-01 12:34:56 -0400

    created QbExporter to pass Query data back to client

---
+++ src/MCBusinessLogic/Controllers/QbExportController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MCBusinessLogic.Controllers.Interfaces;
+using QBConnect;
+
+namespace MCBusinessLogic.Controllers {
+  public class QbExportController : IQbExportController {
+    public QbExportController(string qbFilePath) {
+      QbFilePath = qbFilePath;
+    }
+
+    public string QbFilePath { get; set; }
+
+    public List<string> GetTemplateNamesList() {
+      using (var invoiceImporter = new InvoiceImporter(QbFilePath)) {
+        return invoiceImporter.GetTemplateNamesList();
+      }
+    }
+  }
+}
