1c51b2284d361b0daedf18f7982cd40c19b70806
jamie.sgro <iamjamies@gmail.com>
2020-06-02 16:22:18 -0400

    moved boilerplate properties for Vms into abtract class

---
+++ src/WPFDesktopUI/ViewModels/QuickBooks/AbstractQuickBooksSidePane.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using Caliburn.Micro;
+using stn = WPFDesktopUI.Controllers.SettingsController;
+
+namespace WPFDesktopUI.ViewModels.QuickBooks {
+  public abstract class AbstractQuickBooksSidePane : Screen {
+
+    private string _other;
+    public string Other {
+      get => _other;
+      set {
+        _other = value;
+        NotifyOfPropertyChange(() => Other);
+      }
+    }
+
+    private DateTime _headerDateTextBox = DateTime.Now;
+    public DateTime TxnDate {
+      get => _headerDateTextBox;
+      set {
+        _headerDateTextBox = value;
+        NotifyOfPropertyChange(() => TxnDate);
+      }
+    }
+
+    private string _classRefFullName;
+    public string ClassRefFullName {
+      get => _classRefFullName;
+      set {
+        _classRefFullName = value;
+        NotifyOfPropertyChange(() => ClassRefFullName);
+      }
+    }
+
+    private bool _canTemplateRefFullName = false;
+    public bool CanTemplateRefFullName {
+      get => _canTemplateRefFullName;
+      set {
+        _canTemplateRefFullName = value;
+        NotifyOfPropertyChange(() => CanTemplateRefFullName);
+      }
+    }
+
+    private List<string> _templateRefFullName = new List<string> { "" };
+    public List<string> TemplateRefFullName {
+      get => _templateRefFullName;
+      set {
+        _templateRefFullName = value;
+        NotifyOfPropertyChange(() => TemplateRefFullName);
+      }
+    }
+
+    private string _selectedTemplateRefFullName;
+    public string SelectedTemplateRefFullName {
+      get => _selectedTemplateRefFullName;
+      set {
+        _selectedTemplateRefFullName = value;
+        NotifyOfPropertyChange(() => SelectedTemplateRefFullName);
+      }
+    }
+
+    private bool _hasHeaderOther;
+    public bool HasHeaderOther {
+      get {
+        _hasHeaderOther = stn.QbInvHasHeaderOther();
+        return _hasHeaderOther;
+      }
+    }
+
+    private string _headerOtherTextBlock;
+    public string HeaderOtherTextBlock {
+      get {
+        var name = stn.QbInvHeaderOtherName();
+        _headerOtherTextBlock = HasHeaderOther ? name : "OTHER";
+        return _headerOtherTextBlock;
+      }
+    }
+
+    private bool _canQbExport = true;
+    public bool CanQbExport {
+      get { return _canQbExport; }
+      set {
+        _canQbExport = value;
+        NotifyOfPropertyChange(() => CanQbExport);
+      }
+    }
+
+    private bool _qbProgressBarIsVisible = false;
+    public bool QbProgressBarIsVisible {
+      get => _qbProgressBarIsVisible;
+      set {
+        _qbProgressBarIsVisible = value;
+        NotifyOfPropertyChange(() => QbProgressBarIsVisible);
+      }
+    }
+
+    private bool _canItemRef;
+    public bool CanItemRef {
+      get => _canItemRef;
+      set {
+        _canItemRef = value;
+        NotifyOfPropertyChange(() => CanItemRef);
+      }
+    }
+
+    private List<string> _itemRef;
+    public List<string> ItemRef {
+      get => _itemRef;
+      set {
+        _itemRef = value;
+        NotifyOfPropertyChange(() => ItemRef);
+      }
+    }
+
+    private string _selectedItemRef;
+    public string SelectedItemRef {
+      get { return _selectedItemRef; }
+      set {
+        _selectedItemRef = value;
+        NotifyOfPropertyChange(() => SelectedItemRef);
+      }
+    }
+
+    private string _consoleMessage = "Please select 'Query QuickBooks' before custom lists can be generated";
+    public string ConsoleMessage {
+      get => _consoleMessage;
+      set {
+        _consoleMessage = value;
+        NotifyOfPropertyChange(() => ConsoleMessage);
+      }
+    }
+
+
+
+  }
+}
--- src/WPFDesktopUI/ViewModels/QuickBooks/QuickBooksSidePaneViewModel.cs
+++ src/WPFDesktopUI/ViewModels/QuickBooks/QuickBooksSidePaneViewModel.cs
@@ -9,142 +9,7 @@ using WPFDesktopUI.ViewModels.Interfaces;
 using stn = WPFDesktopUI.Controllers.SettingsController;
 
 namespace WPFDesktopUI.ViewModels.QuickBooks {
-  public class QuickBooksSidePaneViewModel : Screen, IMainTab {
-		private string _otherHeaderTextBlock;
-		private bool _hasHeaderOther;
-		private string _headerOtherTextBox;
-		private DateTime _headerDateTextBox = DateTime.Now;
-    private string _classRefFullName;
-    private List<string> _templateRefFullName = new List<string> {""};
-    private bool _canTemplateRefFullName = false;
-    private string _selectedTemplateRefFullName;
-    private bool _canQbExport = true;
-    private bool _qbProgressBarIsVisible = false;
-    private string _btnNotification = "Please select 'Query QuickBooks' before custom lists can be generated";
-    private List<string> _itemRef;
-    private string _selectedItemRef;
-    private bool _canItemRef;
-
-    public string Other {
-			get => _headerOtherTextBox;
-      set {
-        _headerOtherTextBox = value;
-        NotifyOfPropertyChange(() => Other);
-			}
-		}
-    
-    
-		public DateTime TxnDate {
-			get => _headerDateTextBox;
-      set {
-				_headerDateTextBox = value;
-				NotifyOfPropertyChange(() => TxnDate);
-			}
-		}
-
-    public string ClassRefFullName {
-      get => _classRefFullName;
-      set {
-        _classRefFullName = value;
-        NotifyOfPropertyChange(() => ClassRefFullName);
-			}
-		}
-
-
-    public bool CanTemplateRefFullName {
-      get => _canTemplateRefFullName;
-      set {
-        _canTemplateRefFullName = value;
-        NotifyOfPropertyChange(() => CanTemplateRefFullName);
-      }
-    }
-
-    public List<string> TemplateRefFullName {
-      get => _templateRefFullName;
-      set {
-        _templateRefFullName = value; 
-				NotifyOfPropertyChange(() => TemplateRefFullName);
-      }
-    }
-
-
-    public string SelectedTemplateRefFullName {
-      get => _selectedTemplateRefFullName;
-      set {
-        _selectedTemplateRefFullName = value;
-        NotifyOfPropertyChange(() => SelectedTemplateRefFullName);
-      }
-    }
-
-    public bool HasHeaderOther {
-			get {
-        _hasHeaderOther = stn.QbInvHasHeaderOther();
-        return _hasHeaderOther;
-			}
-		}
-
-		public string HeaderOtherTextBlock {
-			get {
-        var name = stn.QbInvHeaderOtherName();
-        _otherHeaderTextBlock = HasHeaderOther ? name : "OTHER";
-        return _otherHeaderTextBlock;
-      }
-		}
-
-    public bool CanQbExport {
-      get { return _canQbExport; }
-      set {
-        _canQbExport = value;
-        NotifyOfPropertyChange(() => CanQbExport);
-      }
-    }
-
-
-    public bool QbProgressBarIsVisible {
-      get => _qbProgressBarIsVisible;
-      set {
-        _qbProgressBarIsVisible = value;
-        NotifyOfPropertyChange(() => QbProgressBarIsVisible);
-      }
-    }
-
-    public bool CanItemRef {
-      get => _canItemRef;
-      set {
-        _canItemRef = value;
-        NotifyOfPropertyChange(() => CanItemRef);
-      }
-    }
-
-
-    public List<string> ItemRef {
-      get => _itemRef;
-      set {
-        _itemRef = value;
-        NotifyOfPropertyChange(() => ItemRef);
-      }
-    }
-
-    public string SelectedItemRef {
-      get { return _selectedItemRef; }
-      set {
-        _selectedItemRef = value;
-        NotifyOfPropertyChange(() => SelectedItemRef);
-      }
-    }
-
-    private string _consoleMessage;
-
-    public string ConsoleMessage {
-      get => _consoleMessage;
-      set {
-        _consoleMessage = value;
-        NotifyOfPropertyChange(() => ConsoleMessage);
-      }
-    }
-
-
-
+  public class QuickBooksSidePaneViewModel : AbstractQuickBooksSidePane, IMainTab {
 
     public async void OnSelected() {
       await Task.Run(() => {
@@ -155,6 +20,13 @@ namespace WPFDesktopUI.ViewModels.QuickBooks {
       });
     }
 
+    /// <summary>
+    /// Executed when 'Query QuickBooks' button is pressed
+    /// Connect to QB and get all data needed to populate smart dropdowns
+    /// i.e. to decide which template to use, the user should decide from a list
+    /// of actual templates used in QB, thus the combobox needs a list of
+    /// template strings
+    /// </summary>
     public async void QbExport() {
       SessionStart();
       var qbFilePath = stn.QbFilePath();
