c9f05da20126f878f793d665da440aa380a89c85
jamie.sgro <iamjamies@gmail.com>
2020-06-15 16:13:17 -0400

    added abstract class inheriting IRollback for QbVoid

---
+++ src/QBConnect/Classes/Rollback/AbstractRollback.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using QBConnect.Classes.Interfaces;
+using QBConnect.Classes.Rollback.Interfaces;
+using QBFC13Lib;
+
+namespace QBConnect.Classes.Rollback {
+  internal abstract class AbstractRollback : IRollback {
+
+    protected IClientSessionManager _sessionManager { get; set; }
+
+    public void Invoice(string txnId) {
+      var txnIds = new List<string> { txnId };
+      Invoice(txnIds);
+    }
+
+    public void Invoice(List<string> txnIds) {
+      IMsgSetRequest msgSetRequest = GetMsgSetRq();
+
+      //Set field value for TxnID
+      foreach (var txnId in txnIds) {
+        GetTxnRollbackRq(msgSetRequest, txnId);
+      }
+
+      Commit(msgSetRequest);
+    }
+
+    protected abstract void GetTxnRollbackRq(IMsgSetRequest msgSetRequest, string txnId);
+
+    protected IMsgSetRequest GetMsgSetRq() {
+      var req = _sessionManager.CreateMsgSetRequest("US", 13, 0);
+      req.Attributes.OnError = ENRqOnError.roeContinue;
+      return req;
+    }
+
+    protected void Commit(IMsgSetRequest msgSetRequest) {
+      var responseMsgSet = _sessionManager.DoRequests(msgSetRequest);
+
+      // Check all response statuses for potential error
+      for (var i = 0; i < responseMsgSet.ResponseList.Count; i++) {
+        if (responseMsgSet.ResponseList.GetAt(i).StatusMessage != "Status OK") {
+          throw new Exception("Could not void all Invoices. " + responseMsgSet.ResponseList.GetAt(i).StatusMessage);
+        }
+      }
+    }
+  }
+}
--- src/QBConnect/Classes/Rollback/QbVoid.cs
+++ src/QBConnect/Classes/Rollback/QbVoid.cs
@@ -1,43 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using QBConnect.Classes.Interfaces;
-using QBConnect.Classes.Rollback.Interfaces;
+﻿using QBConnect.Classes.Interfaces;
+using QBConnect.Classes.Rollback;
 using QBFC13Lib;
 
 namespace QBConnect.Classes {
-  internal class QbVoid : IRollback {
+  internal sealed class QbVoid : AbstractRollback {
     public QbVoid(IClientSessionManager sessionManager) {
       _sessionManager = sessionManager;
     }
 
-    private IClientSessionManager _sessionManager { get; }
-
-    public void Invoice(string txnId) {
-      var txnIds = new List<string> {txnId};
-      Invoice(txnIds);
-    }
-
-    public void Invoice(List<string> txnIds) {
-      IMsgSetRequest msgSetRequest = GetMsgSetRq();
-
-      //Set field value for TxnID
-      foreach (var txnId in txnIds) {
-        GetTxnVoidRq(msgSetRequest, txnId);
-      }
-
-      Commit(msgSetRequest);
-    }
-
-    private IMsgSetRequest GetMsgSetRq() {
-      var req = _sessionManager.CreateMsgSetRequest("US", 13, 0);
-      req.Attributes.OnError = ENRqOnError.roeContinue;
-      return req;
-    }
-
-    private void GetTxnVoidRq(IMsgSetRequest msgSetRequest, string txnId) {
+    protected override void GetTxnRollbackRq(IMsgSetRequest msgSetRequest, string txnId) {
       // Create new void request
       ITxnVoid txnVoidRq = msgSetRequest.AppendTxnVoidRq();
 
@@ -45,17 +16,6 @@ namespace QBConnect.Classes {
       txnVoidRq.TxnVoidType.SetValue(ENTxnVoidType.tvtInvoice);
 
       txnVoidRq.TxnID.SetValue(txnId);
-    }
-
-    private void Commit(IMsgSetRequest msgSetRequest) {
-      var responseMsgSet = _sessionManager.DoRequests(msgSetRequest);
-
-      // Check all response statuses for potential error
-      for (var i = 0; i < responseMsgSet.ResponseList.Count; i++) {
-        if (responseMsgSet.ResponseList.GetAt(i).StatusMessage != "Status OK") {
-          throw new Exception("Could not void all Invoices. " + responseMsgSet.ResponseList.GetAt(i).StatusMessage);
-        }
-      }
     }
   }
 }
