154438040a625471492c5e37ee78c4a558b37403
jamie.sgro <iamjamies@gmail.com>
2020-06-19 13:56:20 -0400

    added QbDropDown Attribute for attrs with a payload that must be selected from a list

---
+++ src/WPFDesktopUI/Models/SidePaneModels/Attributes/Interfaces/Payloads/IDropDownPayload.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFDesktopUI.Models.SidePaneModels.Attributes.Interfaces.Payloads {
+  public interface IDropDownPayload {
+    bool HasDropDownPayload { get; }
+  }
+}
---
+++ src/WPFDesktopUI/Models/SidePaneModels/Attributes/QbDropDownAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WPFDesktopUI.Models.SidePaneModels.Attributes.Interfaces;
+
+namespace WPFDesktopUI.Models.SidePaneModels.Attributes {
+  class QbDropDownAttribute : QbAbstractAttribute, IQBDropDownAttribute {
+    public QbDropDownAttribute(Func<IQbComboBox> qbComboBox) {
+      DropDownComboBox = qbComboBox();
+    }
+    public IQbComboBox DropDownComboBox { get; set; }
+    public bool HasDropDownPayload { get; } = true;
+
+  }
+}
