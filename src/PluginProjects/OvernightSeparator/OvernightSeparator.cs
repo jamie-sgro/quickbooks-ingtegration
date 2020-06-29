84e5a6644c5f78b1c521883b057e7296d1aa3ed8
jamie.sgro <iamjamies@gmail.com>
2020-06-29 18:02:20 -0400

    added a few empty plugins for the sake of filling out the plugin managers ui for testing

---
+++ src/PluginProjects/OvernightSeparator/OvernightSeparator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InterfaceLibraries;
+
+namespace OvernightSeparator
+{
+  [Export(typeof(IPreprocessor))]
+  [ExportMetadata("Name", "OvernightSeparator")]
+  [ExportMetadata("Author", "Jamie Sgro")]
+  [ExportMetadata("Description", "In CSV import, finds shifts that span several days and separates into new rows for each day")]
+  public class OvernightSeparator : IPreprocessor {
+    public string Preprocess(string dt) {
+      return "This is OvernightSeparator.";
+    }
+  }
+}
---
+++ src/PluginProjects/WeekendFinder/WeekendFinder.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InterfaceLibraries;
+
+namespace WeekendFinder
+{
+  [Export(typeof(IPreprocessor))]
+  [ExportMetadata("Name", "WeekendFinder")]
+  [ExportMetadata("Author", "Jamie Sgro")]
+  [ExportMetadata("Description", "In CSV import, find which rows have dates that occured on a weekend")]
+  public class WeekendFinder : IPreprocessor {
+    public string Preprocess(string dt) {
+      return "This is WeekendFinder.";
+    }
+  }
+}
