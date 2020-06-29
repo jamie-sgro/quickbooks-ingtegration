fdcb6923bbb90fa852b01a4f44669be236026e28
jamie.sgro <iamjamies@gmail.com>
2020-06-29 12:43:38 -0400

    added test MEF code with an MVP

--- src/WPFDesktopUI/ViewModels/ImportViewModel.cs
+++ src/WPFDesktopUI/ViewModels/ImportViewModel.cs
@@ -1,13 +1,20 @@
-﻿using System.ComponentModel;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
 using Caliburn.Micro;
 using MCBusinessLogic.Controllers;
 using System.Data;
 using System.Threading.Tasks;
+using System.ComponentModel.Composition;
+using System.ComponentModel.Composition.Hosting;
+using System.Linq;
+using WPFDesktopUI.Models.ImportModels.Interfaces;
 using WPFDesktopUI.ViewModels.Interfaces;
 
 namespace WPFDesktopUI.ViewModels {
   public class ImportViewModel : Screen, IImportViewModel {
-   
+    [ImportMany(typeof(IPreprocessor), AllowRecomposition = true)]
+    IEnumerable<Lazy<IPreprocessor, IPreprocessorMetaData>> _preprocessors;
 
     public string CsvFilePath { get; set; }
     public DataView CsvDataView { get; set; }
@@ -34,9 +41,42 @@ namespace WPFDesktopUI.ViewModels {
           col.ColumnName = col.ColumnName.Replace("[", "").Replace("]", "");
         }
 
+        Compose();
+
+
+        foreach (Lazy<IPreprocessor, IPreprocessorMetaData> processor in _preprocessors) {
+          if (processor.Metadata.Name == "AlphaProcessor") {
+            Console.WriteLine(processor.Value.Preprocess(""));
+          }
+        }
+
+
+        //Lazy<IPreprocessor, IPreprocessorMetaData> selectedProcessor = _preprocessors.Where(s => (string)s.Metadata.Name == "AlphaProcessor").FirstOrDefault();
+        //selectedProcessor.Value.Preprocess("null");
+        /*foreach (var preprocessor in _preprocessors) {
+          Console.WriteLine(preprocessor.Preprocess("null"));
+        }*/
+
         // Match data structure to the UI view (this lets the user see the data)
         CsvDataView = CsvData.DefaultView;
       });
+    }
+
+    private void Compose() {
+      DirectoryCatalog catalog = new DirectoryCatalog("Plugins", "*.dll");
+      //AssemblyCatalog catalog = new AssemblyCatalog(System.Reflection.Assembly.GetExecutingAssembly());
+      CompositionContainer container = new CompositionContainer(catalog);
+      //container.SatisfyImportsOnce(this);
+      container.ComposeParts(this);
+    }
+
+    [Export(typeof(IPreprocessor))]
+    [ExportMetadata("Name", "BetaProcessor")]
+    [ExportMetadata("IsActive", false)]
+    public class BetaProcessor : IPreprocessor {
+      public string Preprocess(string dt) {
+        return "This is BetaProcessor.";
+      }
     }
   }
 }
