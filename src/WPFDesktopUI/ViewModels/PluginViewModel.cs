ad769ee28160ac865f64386e310c248aca0859f9
jamie.sgro <iamjamies@gmail.com>
2020-06-30 10:15:41 -0400

    save state of plugin VM before bug testing

--- src/WPFDesktopUI/ViewModels/PluginViewModel.cs
+++ src/WPFDesktopUI/ViewModels/PluginViewModel.cs
@@ -1,22 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
+﻿using System.Collections.ObjectModel;
 using System.ComponentModel;
-using System.ComponentModel.Composition;
-using System.ComponentModel.Composition.Hosting;
-using System.Linq;
-using System.Security.Cryptography;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Controls;
 using Caliburn.Micro;
-using InterfaceLibraries;
-using MCBusinessLogic.DataAccess;
-using WPFDesktopUI.Models.CustomerModels;
-using WPFDesktopUI.Models.CustomerModels.Interfaces;
-using WPFDesktopUI.Models.DbModels.Interfaces;
 using WPFDesktopUI.Models.PluginModels;
-using WPFDesktopUI.Models.PluginModels.Interfaces;
 using WPFDesktopUI.ViewModels.Interfaces;
 
 namespace WPFDesktopUI.ViewModels {
@@ -28,7 +13,6 @@ namespace WPFDesktopUI.ViewModels {
     }
 
     public IPluginModel PluginModel { get; set; }
-    //public DataGrid PluginDataGrid { get; set; }
     public ObservableCollection<ClientPlugin> ReactiveCollection { get; set; }
     public string Title { get; set; } = "Plugin Manager";
 
