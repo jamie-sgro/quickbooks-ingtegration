ad41c5373e975f5185f421380f62bb545096988c
jamie.sgro <iamjamies@gmail.com>
2020-07-02 14:33:21 -0400

    updated weekendfinder to append a new column with an indicator for WKD

--- src/PluginProjects/WeekendFinder/WeekendFinder.cs
+++ src/PluginProjects/WeekendFinder/WeekendFinder.cs
@@ -16,7 +16,25 @@ namespace WeekendFinder
   [ExportMetadata("Description", "In CSV import, find which rows have dates that occured on a weekend")]
   public class WeekendFinder : IPreprocessor {
     public DataTable Preprocess(DataTable dt) {
-      return null;
+      if (dt.Columns.Contains("Date") == false) return dt;
+      if (dt.Columns.Contains("Position") == false) return dt;
+
+      dt.Columns.Add("PositionWkd", typeof(string));
+      dt.Columns["PositionWkd"].Expression = null;
+      dt.Columns["PositionWkd"].ReadOnly = false;
+
+      foreach (DataRow row in dt.Rows) {
+        var day = Convert.ToDateTime(row["Date"]);
+
+
+        if ((day.DayOfWeek == DayOfWeek.Saturday) || (day.DayOfWeek == DayOfWeek.Sunday)) {
+          row["PositionWkd"] = row["Position"] + " - WKD";
+        } else {
+          row["PositionWkd"] = row["Position"];
+        }
+      }
+
+      return dt;
     }
   }
 }
