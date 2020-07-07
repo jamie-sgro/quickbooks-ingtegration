21672339084d717af4ac2b0cb2a9e428ae14ae92
jamie.sgro <iamjamies@gmail.com>
2020-07-07 17:04:51 -0400

    modified plugin view to show description in side pane

--- src/WPFDesktopUI/ViewModels/PluginViewModel.cs
+++ src/WPFDesktopUI/ViewModels/PluginViewModel.cs
@@ -1,8 +1,11 @@
-﻿using System.Collections.ObjectModel;
+﻿using System;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using Caliburn.Micro;
 using WPFDesktopUI.Controllers;
+using WPFDesktopUI.Models.ItemReplacerModels.Interfaces;
 using WPFDesktopUI.Models.PluginModels;
+using WPFDesktopUI.Models.PluginModels.Interfaces;
 using WPFDesktopUI.ViewModels.Interfaces;
 
 namespace WPFDesktopUI.ViewModels {
@@ -17,6 +20,7 @@ namespace WPFDesktopUI.ViewModels {
     public IPluginModel PluginModel { get; set; }
     public ObservableCollection<ClientPlugin> ReactiveCollection { get; set; }
     public string Title { get; set; } = "Plugin Manager";
+    public string PluginDescription { get; set; } = "";
 
 
 
@@ -34,6 +38,26 @@ namespace WPFDesktopUI.ViewModels {
       PluginModel.Update(ReactiveCollection);
       Title = Title.Replace("*", "");
       CanBtnUpdate = false;
+    }
+
+    /// <summary>
+    /// Fires when the datagrid / listview is selected.
+    /// Updates the extention textbox with the detail
+    /// property of this model
+    /// </summary>
+    /// <param name="ClientPluginObj">
+    /// A single ClientPlugin indicating which DataGrid row was selected
+    /// If whitespace was selected instead, indicates the last selected /
+    /// currently active DataGrid Row
+    /// </param>
+    public void OnCellsChanged(object ClientPluginObj) {
+      var IsValidType = ClientPluginObj is IClientPlugin;
+      if (!IsValidType) {
+        throw new ArgumentException(@"OnCellsChanged() parameter not of type " + typeof(IClientPlugin), nameof(ClientPluginObj));
+      }
+
+      var clientPlugin = (IClientPlugin)ClientPluginObj;
+      PluginDescription = clientPlugin.Description;
     }
 
     private static readonly log4net.ILog log = LogHelper.GetLogger();
