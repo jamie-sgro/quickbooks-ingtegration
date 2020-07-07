e8bb4287dfe94b4cd3f528c48ff7fc6c9af26ef6
jamie.sgro <iamjamies@gmail.com>
2020-07-07 11:12:21 -0400

    updated item replacer with linq and full sql-Like table structure

--- src/WPFDesktopUI/ViewModels/ItemViewModel.cs
+++ src/WPFDesktopUI/ViewModels/ItemViewModel.cs
@@ -12,61 +12,66 @@ using WPFDesktopUI.ViewModels.Interfaces;
 namespace WPFDesktopUI.ViewModels {
   public class ItemViewModel : Screen, IItemViewModel {
     private string _replacer;
+    private ObservableCollection<ItemReplacer> _uniqueReplaceWith;
 
     public ItemViewModel() {
-      ItemReplacer = new Dictionary<string, ObservableCollection<SimpleStr>> {
-        {
-          "PSW", new ObservableCollection<SimpleStr> {
-          new SimpleStr{ Payload = "Barrie Connie Thompson- PSW"},
-          new SimpleStr { Payload = "CLASS - DSW1" }
-          }
-        },{
-        "RN", new ObservableCollection<SimpleStr> {
-          new SimpleStr{ Payload = "Barrie Connie Thompson- RN"},
-          new SimpleStr { Payload = "CLASS - DSW1" }
-        }},
+      ItemReplacers = new ObservableCollection<ItemReplacer> {
+        new ItemReplacer() {ReplaceWith = "PSW", ToReplace = "Barrie Connie Thompson- PSW"},
+        new ItemReplacer() {ReplaceWith = "PSW", ToReplace = "CLASS - PSW1"},
+        new ItemReplacer() {ReplaceWith = "RN",  ToReplace = "Barrie Connie Thompson- RN"},
+        new ItemReplacer() {ReplaceWith = "RN",  ToReplace = "CLASS - RN1"}
       };
     }
 
     public string SelectedKey { get; set; } = null;
 
-    /// <summary>
-    /// Property that lets the user edit the values inside ItemReplacer
-    /// </summary>
-    public ObservableCollection<SimpleStr> Replacer {
-      get => ItemReplacer[SelectedKey];
-      set => ItemReplacer[SelectedKey] = value;
-    }
 
-    public struct SimpleStr {
-      public string Payload { get; set; }
-    }
-
-    public List<string> TempList { get; set; } = new List<string>{"a","b","c"};
-    public TextBox ReplacerTextBox { get; set; }
-
-    public Dictionary<string, ObservableCollection<SimpleStr>> ItemReplacer { get; set; }
+    public ObservableCollection<ItemReplacer> ItemReplacers { get; set; }
     public string TabHeader { get; set; } = "Item";
+
+    public ObservableCollection<ItemReplacer> SelectedItem { get; set; }
+
+    public ObservableCollection<ItemReplacer> UniqueReplaceWith {
+      get {
+        return new ObservableCollection<ItemReplacer>(ItemReplacers
+          .GroupBy(x => x.ReplaceWith)
+          .Select(x => x.First())
+          .ToList());
+      }
+      set => _uniqueReplaceWith = value;
+    }
 
     public void OnSelected() {
     }
 
     public void OnKeyUp(object itemReplacerObj) {
-      var isDict = itemReplacerObj is KeyValuePair<string, ObservableCollection<SimpleStr>>;
+      var isDict = itemReplacerObj is ItemReplacer;
       if (! isDict) {
-        throw new ArgumentException(@"OnKeyUp() parameter not of type KeyValuePair", nameof(itemReplacerObj));
+        throw new ArgumentException(@"OnKeyUp() parameter not of type " + typeof(ItemReplacer), nameof(itemReplacerObj));
       }
 
       // Cast to KeyValuePair like Dict
-      var itemReplacer = (KeyValuePair<string, ObservableCollection<SimpleStr>>)itemReplacerObj;
-      SelectedKey = itemReplacer.Key;
+      var itemReplacer = (ItemReplacer)itemReplacerObj;
+      SelectedKey = itemReplacer.ReplaceWith;
 
-      // Temp
-      var a = ItemReplacer;
+      // Update SelectedItem
+      SelectedItem = new ObservableCollection<ItemReplacer>(ItemReplacers
+        .Where(x => x.ReplaceWith == SelectedKey)
+        .ToList());
     }
 
     public void OnCellEditEnding() {
     }
 
+  }
+
+  public class ItemReplacer : IItemReplacer {
+    public string ReplaceWith { get; set; }
+    public string ToReplace { get; set; }
+  }
+
+  public interface IItemReplacer {
+    string ReplaceWith { get; set; }
+    string ToReplace { get; set; }
   }
 }
