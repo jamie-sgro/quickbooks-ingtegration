4f3c9c1f6bea23e4775a25c287a14f11d15b30c1
jamie.sgro <iamjamies@gmail.com>
2020-08-31 10:59:22 -0400

    added new plugin for after group by

---
+++ src/SumQuantityToFOB/SumQuantityToFOB.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InterfaceLibraries;
+using MCBusinessLogic.Models.Interfaces;
+
+namespace SumQuantityToFOB
+{
+  [Export(typeof(IPreprocessor))]
+  [Export(typeof(IPlugin))]
+  [ExportMetadata("Name", "SumQuantityToFOB")]
+  [ExportMetadata("Author", "Jamie Sgro")]
+  [ExportMetadata("Description", "After data has been grouped into separate invoices," +
+                                 " modify the FOB header box to equal the sum of all" +
+                                 " quantities in the line item section for each invoice")]
+  public class SumQuantityToFOB : IAfterGroupBy {
+    public List<IInvoice> ModifyGrouped(List<IInvoice> groupBy) {
+      Console.WriteLine("The plugin was accessed correct;y");
+      throw new NotImplementedException();
+    }
+  }
+}
